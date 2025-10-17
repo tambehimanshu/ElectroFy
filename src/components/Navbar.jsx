a9cdd3fdaec488b6ef2211c41d7d915447e961e6
tambehimanshu <tambehimanshu18>
2025-10-18 01:06:45 +0530

    initilizations

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+  <header className='flex justify-between items-center py-4 px-4 '>
+    <h1>ElectroFy</h1>
+
+    {/*desktop navigation  */}
+    <nav>
+        <ul>
+            <li>
+               
+            </li>
+        </ul>
+    </nav>
+
+  </header>
+  )
+}
+
+export default Navbar
